a7362281d07d90e9b17eef34ea205f3bccca2708
Gabriel Guerra <gfranklings@gmail.com>
2022-08-06 17:18:58 -0400

    managing rounded borders using picom instead of gears.shape.rounded_rect

--- cfg/awesome/ui/maximization-manage.lua
+++ cfg/awesome/ui/maximization-manage.lua
@@ -2,17 +2,6 @@
 local gears = require "gears"
 local beautiful = require "beautiful"
 
--- apply border radius
-client.connect_signal('request::manage', function (c)
-    if not c.maximized then
-        c.shape = function (cr, w, h)
-            return gears.shape.rounded_rect(cr, w, h, beautiful.border_radius)
-        end
-    else
-        c.shape = gears.shape.rectangle
-    end
-end)
-
 -- maximize wibar when a client is maximized
 client.connect_signal('request::border', function (c)
     if c.maximized then
