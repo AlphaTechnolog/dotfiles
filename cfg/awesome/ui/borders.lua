32b00fa780139ba84d7c2561556b03a22c9bbbdc
Gabriel Guerra <gfranklings@gmail.com>
2022-08-07 12:07:33 -0400

    AwesomeWM: borders.lua: some code improvements

--- cfg/awesome/ui/borders.lua
+++ cfg/awesome/ui/borders.lua
@@ -2,26 +2,39 @@
 local gears = require "gears"
 local beautiful = require "beautiful"
 
-local borders = {}
+-- apply rounded borders to the given client, also checks the maximized state
+local function rounded_borders(c)
+    c.shape = c.maximized and gears.shape.rectangle or function (cr, w, h)
+        return gears.shape.rounded_rect(cr, w, h, beautiful.border_radius)
+    end
+end
 
-function borders.setup (c)
+-- readjust the wibar if the given client is maximized or not
+local function readjust_wibar_geometry(c)
+    local screen = c.screen
+    local mywibox = screen.mywibox
+
     if c.maximized then
-        c.shape = gears.shape.rectangle
-        c.screen.mywibox.width = c.screen.geometry.width
-        c.screen.mywibox.shape = gears.shape.rectangle
+        mywibox.width = screen.geometry.width
+        mywibox.shape = gears.shape.rectangle
     else
-        c.shape = function (cr, width, height)
-            return gears.shape.rounded_rect(cr, width, height, beautiful.border_radius)
-        end
-
-        c.screen.mywibox.width = c.screen.geometry.width - beautiful.useless_gap * 1.5
-        c.screen.mywibox.shape = function (cr, width, height)
-            return gears.shape.partially_rounded_rect(cr, width, height, true, true, false, false, beautiful.border_radius)
+        mywibox.width = screen.geometry.width - beautiful.useless_gap * 1.5
+        mywibox.shape = function (cr, w, h)
+            return gears.shape.partially_rounded_rect(cr, w, h, true, true, false, false, beautiful.border_radius)
         end
     end
 end
 
-client.connect_signal('request::manage', borders.setup)
-client.connect_signal('request::border', borders.setup)
+-- applying rounded corners when a new client is added
+client.connect_signal('request::manage', rounded_borders)
 
-return borders
+-- applying rounded corners to the existent clients (useful when reloading AwesomeWM)
+for _, c in ipairs(client.get()) do
+    rounded_borders(c)
+end
+
+-- readjusting wibar dimensions when the client border is updated
+client.connect_signal('request::border', function (c)
+    rounded_borders(c)
+    readjust_wibar_geometry(c)
+end)
