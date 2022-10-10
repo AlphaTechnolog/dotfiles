927558188b050ef2406ea16869616c4b471abf89
Gabriel Guerra <gfranklings@gmail.com>
2022-10-10 10:45:02 -0400

    using an external bash script to avoid issues when running with anothers shells like fish.

---
+++ cfg/awesome/scripts/volume.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SINK=$(pactl list short sinks | sed -e 's,^\\([0-9][0-9]*\\)[^0-9].*,\\1,' | head -n 1 | awk '{print $1}');
+
+get () {
+  pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | awk '{print $5}' | tr '%' ' ' | xargs
+}
+
+if [[ $1 == "get" ]]; then
+  get
+fi
--- cfg/awesome/signal/volume.lua
+++ cfg/awesome/signal/volume.lua
@@ -9,12 +9,11 @@ local helpers = require 'helpers'
 
 local volume = {}
 
-local sink_part = "SINK=$(pactl list short sinks | sed -e 's,^\\([0-9][0-9]*\\)[^0-9].*,\\1,' | head -n 1 | awk '{print $1}');"
-local script = sink_part .. "pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \\([0-9][0-9]*\\)%.*,\\1,'"
+local script = gfs.get_configuration_dir() .. 'scripts/volume.sh'
 local how_to_know_if_muted = "pacmd list-sinks | awk '/muted/ { print $2 }'"
 
 function volume.re_emit_volume_value_signal()
-    awful.spawn.easy_async_with_shell(script, function(out)
+    awful.spawn.easy_async_with_shell(script .. ' get', function (out)
         awesome.emit_signal('volume::value', tonumber(out))
     end)
 end
